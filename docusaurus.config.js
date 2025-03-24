f32178423c0c9001c3380dc2392347d2711acabc
Diego <2248356998@qq.com>
2025-03-24 10:37:34 +0800

    更新文档

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,16 @@ const config = {
       },
     },
   },
-
+  plugins: [
+    [
+      'vercel-analytics',
+      {
+        debug: true,
+        mode: 'auto',
+      },
+    ],
+  ],
+  
   presets: [
     [
       'classic',
